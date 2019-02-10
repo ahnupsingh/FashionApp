f72364cb72bb2bfa223619a51b8c74d059c11e34
ahnupsingh <anup.singh2071@gmail.com>
2019-02-10 18:09:03 +0545

    Sign up functionality implemented for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,29 @@
 class UsersController < ApplicationController
+
+  def index
+
+  end
+
   def new
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
-    binding.pry
   end
+
+  def create
+    @user = User.new(user_params[:user])
+    if @user.save
+      # Handle a successful save.
+      # render 'show'
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
