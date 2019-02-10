75fff79c18775026e8741de7cdce530bd189d0b2
ahnupsingh <anup.singh2071@gmail.com>
2019-02-11 03:56:16 +0545

    Login functionality for user added using session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: session_params[:email].downcase)
+    if user && user.authenticate(session_params[:password])
+      # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user
+    else
+      # Create an error message.
+       flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to login_path
+  end
+
+  private
+    def session_params
+      params.require(:session).permit(:email, :password)
+    end
+end
