ed3b9ce32d00674c77b4c50d89aa0c6102033332
ahnupsingh <anup.singh2071@gmail.com>
2019-02-11 10:19:36 +0545

    Implemented remember_digest in user model to store encrypted remember token

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,13 +7,43 @@ module SessionsHelper
 
   # Returns the current logged-in user (if any)
   def current_user
+    # retrieve the user from session if exist else look for cookies[:user_id]
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
+    elsif cookies.signed[:user_id]
+      # cookies.signed[:user_id] decrypts the user_id stored in cookie
+      user = User.find_by(id: cookies.signed[:user_id])
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
   def logged_in?
     !current_user.nil?
+  end
+
+  def remember(user)
+    user.remember
+    # permanent -> destroy after 20 years from now
+    # signed -> store user_id as encrypted cookie
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # Logs out the current user.
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
   end
 
 end
